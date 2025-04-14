374a33791bc801bc23611997272b8b5a3aa07b90
YcaroMiguel <144180288+YcaroMiguel@users.noreply.github.com>
2025-04-14 10:13:51 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const alunos = [
+    "Aimê Laís", "Alanna Gabrielly", "Alice Feitosa", "Ana Clara", "Ana Sophia",
+    "André Lucas", "Ashira Giovanna", "Bruna Vitória", "Cesar Miguel", "Elias Maia",
+    "Emanuelly Ester", "Felipe de Melo", "Gabriel Pinheiro", "Hiel Alves", "Isabelly Lins",
+    "João Lucas", "Júlio Cezar", "Kaira Vitória", "Luiz Gabriel", "Lyvia Emanuelly",
+    "Maria Alice", "Maria Eduarda", "Marya Luiza", "Matheus Vitor", "Pedro Henrique",
+    "Rebecca Silva", "Samuel Asafe", "Sury de França", "Thais Lopez", "Ycaro Miguel"
+  ];
+  
+  const selectAluno = document.getElementById('aluno');
+  
+  alunos.sort().forEach(nome => {
+    const option = document.createElement('option');
+    option.value = nome;
+    option.textContent = nome;
+    selectAluno.appendChild(option);
+  });
+  
