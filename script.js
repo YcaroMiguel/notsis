c443b33d076f2abe315b406a2ed3bd59bff55ac9
YcaroMiguel <144180288+YcaroMiguel@users.noreply.github.com>
2025-04-15 12:43:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const alunos = [
     "Aimê Laís", "Alanna Gabrielly", "Alice Feitosa", "Ana Clara", "Ana Sophia",
     "André Lucas", "Ashira Giovanna", "Bruna Vitória", "Cesar Miguel", "Elias Maia",
-    "Emanuelly Ester", "Felipe de Melo", "Gabriel Pinheiro", "Hiel Alves", "Isabelly Lins",
+    "Emanuelly Ester", "Felipe de Melo", "Gabriel Pinheiro", "Gustavo Luiz", "Hiel Alves", "Isabelly Lins",
     "João Lucas", "Júlio Cezar", "Kaira Vitória", "Luiz Gabriel", "Lyvia Emanuelly",
     "Maria Alice", "Maria Eduarda", "Marya Luiza", "Matheus Vitor", "Pedro Henrique",
     "Rebecca Silva", "Samuel Asafe", "Sury de França", "Thais Lopez", "Ycaro Miguel"
